0800d253ed36fc699379d1ff3c50f13e1a60fcc2
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-11-29 21:44:14 +0600

    UI Design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,15 @@ import SearchIcon from '@mui/icons-material/Search';
 import InboxIcon from '@mui/icons-material/Inbox';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Avatar } from '@mui/material';
+import diuMate from './../../image/diuMate.gif';
 
 const Header = () => {
   return (
     <header>
       <div className='header-container'>
         <div className='header-left'>
-          <h2>DIUMate</h2>
+          {/* <h2>DIUMate</h2> */}
+          <img src={diuMate} alt='Logo' />
           <h3>Products</h3>
         </div>
         <div className='header-middle'>
