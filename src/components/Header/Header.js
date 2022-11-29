cd21aa1ba2e6eccdbe3e6220a4e81b3073ca870d
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-11-29 15:13:20 +0600

    Header Component Design Done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import InboxIcon from '@mui/icons-material/Inbox';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Avatar } from '@mui/material';
 
 const Header = () => {
   return (
-    <div>
-      <h3>Header Component</h3>
-    </div>
+    <header>
+      <div className='header-container'>
+        <div className='header-left'>
+          <h1>DIUMate</h1>
+          <h3>Products</h3>
+        </div>
+        <div className='header-middle'>
+          <div className='header-search-container'>
+            <SearchIcon />
+            <input type='search' name='search' placeholder='Search...' />
+          </div>
+        </div>
+        <div className='header-right'>
+          <div className='header-right-container'>
+            <Avatar />
+            <InboxIcon />
+            <MenuIcon />
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
